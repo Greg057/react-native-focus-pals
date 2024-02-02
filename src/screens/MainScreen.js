141f5d09048dc7fe48556228c7d8a36f9448bff6
Greg <88341092+Greg057@users.noreply.github.com>
2024-02-02 17:45:52 +0100

    Add main screen and eslint

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import SliderTimer from '../components/SliderTimer'
+import CountdownTimer from '../components/CountdownTimer'
+
+export default function MainScreen () {
+    const [timer, setTimer] = useState(5)
+    const [isTimerHidden, setIsTimerHidden] = useState(true)
+    const [timeFocused, setTimeFocused] = useState(null)
+
+    return (
+        <View style={styles.container}>
+            {timeFocused && <Text>You focused for {timeFocused} minutes!</Text>}
+
+            {isTimerHidden ? (
+                <View>
+                    <Text>{timer}</Text>
+                    <SliderTimer timer={timer} setTimer={setTimer}/>
+                    <Pressable onPress={() => {
+                        setTimeFocused(null)
+                        setIsTimerHidden(false)
+                        }
+                    } 
+                    style={{backgroundColor: "lightblue"}}>
+                        <Text>Start</Text>
+                    </Pressable>
+                </View>
+                ) : (
+                    <CountdownTimer timer={timer} setTimeFocused={setTimeFocused} setIsTimerHidden={setIsTimerHidden} />
+                )}
+
+        <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    padding: 30
+  },
+});
