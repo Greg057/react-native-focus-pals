e686038699eac4ee857994d6f144c9c66c3006d1
Greg <88341092+Greg057@users.noreply.github.com>
2024-02-02 18:08:25 +0100

    Change style

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -2,18 +2,17 @@ import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 import { Text } from 'react-native'
 
 export default function CountdownTimer ({timer, setTimeFocused, setIsTimerHidden}) {
-    return (
-        <CountdownCircleTimer
-        isPlaying
-        duration={timer}
-        colors={'#004777'}
-        rotation="counterclockwise"
-        onComplete={(value) => {
-          setTimeFocused(value)
-          setIsTimerHidden(true)
-        }}
-      >
-        {({ remainingTime }) => <Text>{remainingTime}</Text>}
-      </CountdownCircleTimer>
-    )
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={timer}
+      colors={'#004777'}
+      rotation="counterclockwise"
+      onComplete={(value) => {
+        setTimeFocused(value)
+        setIsTimerHidden(true)
+      }}>
+      {({ remainingTime }) => <Text>{remainingTime}</Text>}
+    </CountdownCircleTimer>
+  )
 }
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -5,34 +5,33 @@ import SliderTimer from '../components/SliderTimer'
 import CountdownTimer from '../components/CountdownTimer'
 
 export default function MainScreen () {
-    const [timer, setTimer] = useState(5)
-    const [isTimerHidden, setIsTimerHidden] = useState(true)
-    const [timeFocused, setTimeFocused] = useState(null)
+	const [timer, setTimer] = useState(5)
+	const [isTimerHidden, setIsTimerHidden] = useState(true)
+	const [timeFocused, setTimeFocused] = useState(null)
 
-    return (
-        <View style={styles.container}>
-            {timeFocused && <Text>You focused for {timeFocused} minutes!</Text>}
+	return (
+		<View style={styles.container}>
+			{timeFocused && <Text>You focused for {timeFocused} minutes!</Text>}
 
-            {isTimerHidden ? (
-                <View>
-                    <Text>{timer}</Text>
-                    <SliderTimer timer={timer} setTimer={setTimer}/>
-                    <Pressable onPress={() => {
-                        setTimeFocused(null)
-                        setIsTimerHidden(false)
-                        }
-                    } 
-                    style={{backgroundColor: "lightblue"}}>
-                        <Text>Start</Text>
-                    </Pressable>
-                </View>
-                ) : (
-                    <CountdownTimer timer={timer} setTimeFocused={setTimeFocused} setIsTimerHidden={setIsTimerHidden} />
-                )}
+			{isTimerHidden ? (
+				<View>
+					<Text>{timer}</Text>
+					<SliderTimer timer={timer} setTimer={setTimer}/>
+					<Pressable onPress={() => {
+							setTimeFocused(null)
+							setIsTimerHidden(false)
+							}} 
+						style={{backgroundColor: "lightblue"}}>
+							<Text>Start</Text>
+					</Pressable>
+				</View>
+				) : (
+					<CountdownTimer timer={timer} setTimeFocused={setTimeFocused} setIsTimerHidden={setIsTimerHidden} />
+				)}
 
-        <StatusBar style="auto" />
-        </View>
-    )
+		<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
