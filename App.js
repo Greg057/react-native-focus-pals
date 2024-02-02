6d91567bd68014d03e429268f11c1c649711fc7d
Greg <88341092+Greg057@users.noreply.github.com>
2024-02-02 19:52:52 +0100

    Add navigation - Tabs

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import MainScreen from "./src/screens/MainScreen";
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import MainScreen from "./src/screens/MainScreen"
+import ShopScreen from "./src/screens/ShopScreen"
+import CollectionScreen from "./src/screens/CollectionScreen"
+import { Image } from "react-native"
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <MainScreen />
+    <NavigationContainer>
+      <Tab.Navigator  screenOptions={{tabBarShowLabel: false, tabBarStyle: {height: 60}}}>
+        <Tab.Screen name="Shop" component={ShopScreen} options={{tabBarIcon: () => <Image style={{ width: 60, height: 60}} source={require("./assets/images/shopIconNav.png")}/>}} />
+        <Tab.Screen name="Timer" component={MainScreen} options={{tabBarIcon: () => <Image style={{ width: 60, height: 60}} source={require("./assets/images/homeIconNav.png")}/>}} />
+        <Tab.Screen name="Collection" component={CollectionScreen} options={{tabBarIcon: () => <Image style={{ width: 60, height: 60}} source={require("./assets/images/collectionIconNav.png")}/>}} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
