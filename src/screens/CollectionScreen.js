d3bf4b48e2bf8137042b041d3363b66f372b19ca
Greg <88341092+Greg057@users.noreply.github.com>
2024-02-05 13:19:44 +0100

    display collection pets

--- src/screens/CollectionScreen.js
+++ src/screens/CollectionScreen.js
@@ -2,29 +2,42 @@ import { Image, Text, View } from "react-native"
 import Header from "../components/Header"
 import { doc, getDoc } from "firebase/firestore"
 import { FIREBASE_DB } from "../../firebaseConfig"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 export default function CollectionScreen () {
+	const [petsOwned, setPetsOwned] = useState([])
 
-	async function getImg () {
-		const docRef = await getDoc(doc(FIREBASE_DB, "pets", "pet1"))
-		const url = docRef.data().image
-		return url
-		
-	}
-	async function test () {
-		const res = await getImg()
-		console.log(res)
-	} 
+	useEffect(() => {
+		async function getPets() {
+			const docRef = await getDoc(doc(FIREBASE_DB, "users", getAuth().currentUser.uid));
+			const pets = docRef.data().petsOwned;
+	
+			const petPromises = pets.map(async (pet) => {
+				const petRef = await getDoc(doc(FIREBASE_DB, "pets", pet));
+				return petRef.data();
+			});
+	
+			const petData = await Promise.all(petPromises);
+	
+			setPetsOwned(petData);
+			console.log(petData);
+		}
+		getPets()
+	}, [])
+
 	
 
-	test()
+	
+	
 	
 
 	return (
 		<View style={{flex: 1, padding: 15}}>
 			<Header />
 			<Text>Collection</Text>
-			<Image style={{width: 100, height: 100}} source={null} />
+			{petsOwned.map(pet => <Image style={{width: 100, height: 100}} source={{ uri: pet.image}} key={pet.name} /> )}
+			
 		</View>
 	)
 }
